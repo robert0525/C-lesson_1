61efa6bd2cef82a2b20ea407512f205a93cfff94
Robert Rydlewski <robertrydlewski1984@gmail.com>
2020-02-23 18:29:15 +0100

    first commit

--- Program.cs
+++ Program.cs
@@ -1,7 +1,11 @@
+using System;
+using System.Threading; 
 class Program
 {
     static void Main(string[] args)
     {
-        System.Console.WriteLine("Welcome to the cat food store!");
+        Console.WriteLine("Welcome to the cat food store!");
+        Thread.Sleep(3000);
+        Console.WriteLine("done sleeping");
     }
 }
